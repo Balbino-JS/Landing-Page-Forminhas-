dff3c5dccdcd8ddfe5c6fde887aa2ee4c97116cd
Balbino-teste <balbinojteste@gmail.com>
2025-04-17 19:44:00 -0300

    alterações
    alterações entre html ,css e js, também adicionado algumas imagens.

--- src/script/script.js
+++ src/script/script.js
@@ -5,5 +5,19 @@ $(document).ready(() =>{
         $('#mobile_btn').find('i').toggleClass('fa-x');
         
     });
+
+    const img = document.querySelector('.img-profile');
+
+img.addEventListener('click', () => {
+    // Se já está com zoom, não reaplica
+    if (!img.classList.contains('img-zoom')) {
+        img.classList.add('img-zoom');
+        setTimeout(() => {
+            img.classList.remove('img-zoom');
+        }, 2000); // 2 segundos
+    }
 });
+});
+
+
 
