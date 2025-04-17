a223df275020db28e9777b9e878084155e6181ef
Balbino-teste <balbinojteste@gmail.com>
2025-04-17 11:40:00 -0300

    Iniciando o css
    comecei a iniciar o css, e tambeém realizei algumas alterações no html como class mudanças de posições de botões e textos.

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,9 @@
 /* Usaremos jQuery para a função de movimento do icone fa-bars*/
-$(doument).ready(function(){
-    $('#mobile_btn').on('click', function(){
+$(document).ready(() =>{
+    $('#mobile_btn').on('click', () => {
         $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x')
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+        
     });
 });
+
