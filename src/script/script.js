e89c60e41082feba64db6d261f319ccfe1bc7104
Balbino-teste <balbinojteste@gmail.com>
2025-04-19 10:40:25 -0300

    Criação da section Catálogo
    Nessa section foi criado toda a estrutura e adicionado script para o carrossel  e as imagens

--- src/script/script.js
+++ src/script/script.js
@@ -19,5 +19,27 @@ img.addEventListener('click', () => {
 });
 });
 
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
 
 
